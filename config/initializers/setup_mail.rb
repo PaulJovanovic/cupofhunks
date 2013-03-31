d17009a6a85f1a5e33a0575ed9fcae7a6a30a7a3
Pavle Jovanovic <pavlej@vailsys.com>
2013-03-30 21:30:32 -0500

    testing

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,10 +1,10 @@
 ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => "cupofhunks.com",
-  :user_name            => "cupofhunks",
-  :password             => "Sept1990",
-  :authentication       => "plain",
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
   :enable_starttls_auto => true
 }
 

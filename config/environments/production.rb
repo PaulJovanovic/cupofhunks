62fba81616f3625396427829c28970fa0b9dc57f
Pavle Jovanovic <pavlej@vailsys.com>
2013-03-30 20:42:58 -0500

    yee

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,4 @@ Cup::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-
-  config.action_mailer.perform_deliveries = true
 end

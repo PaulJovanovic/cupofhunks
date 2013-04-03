c527613953e2af0a72463c5fb551453dd2882c31
Pavle Jovanovic <pavlej@vailsys.com>
2013-04-03 15:23:58 -0500

    stuff

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,5 +2,5 @@ class Photo < ActiveRecord::Base
 	belongs_to :project
 	attr_accessible :image, :project_id
 	validates_attachment_presence :image
-	has_attached_file :image, :styles => { :large => "884x884", :small => "278x278", :thumb => "100x100>" }, :url => ":s3_domain_url", :path => "/:class/images/:id_:basename.:style.:extension"
+	has_attached_file :image, :styles => { :large => "884x884", :small => "278x278", :thumb => "100x100" }, :url => ":s3_domain_url", :path => "/:class/images/:id_:basename.:style.:extension"
 end
